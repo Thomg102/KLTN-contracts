702addbd1080134ae0580626568f8e39f1b51232
LocNX <locnguyen.lotussoft.vn>
2022-03-07 23:02:11 +0700

    Update ISharedStruct

--- contracts/common/interfaces/ISharedStruct.sol
+++ contracts/common/interfaces/ISharedStruct.sol
@@ -5,5 +5,11 @@ interface ISharedStruct {
     struct NFTInfo {
         string metadataInHash;
         string moreInfo;
+        bool isActive;
     }
 }
+
+
+// check if the NFT is Course of Items:
+// string memory empty = "";
+// keccak256(bytes(_nftInfo.moreInfo)) != keccak256(bytes(empty));
